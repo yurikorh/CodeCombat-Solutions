e01e79e2af126c5fe96dcc5b6a274ad58646cfbd
yurikorh <33363503+yurikorh@users.noreply.github.com>
2021-11-29 22:58:18 +0800

    Update SarvenSavior.py

--- Desert/SarvenSavior/SarvenSavior.py
+++ Desert/SarvenSavior/SarvenSavior.py
@@ -2,21 +2,9 @@
 # Этот массив - список имен твоих друзей.
 friendNames = ['Joan', 'Ronan', 'Nikita', 'Augustus']
 
-# Индекс массива начинается с 0, а не с 1!
-friendIndex = 0
-
-# Включи в цикл все имена в массиве.
-# Функция len() возвращает длину списка.
-while friendIndex < len(friendNames):
-    # Используй квадратные скобки чтобы извлечь имя из массива.
-    friendName = friendNames[friendIndex]
-
-    # Прикажи другу идти домой.
-    # Используй + для объединения двух строковых элементов.
+for friendName in friendNames:
     hero.say(friendName + ', go home!')
-
-    # Увеличивай индекс, чтобы получить следующее имя из массива.
-    friendIndex += 1
-hero.moveXY(25, 30)
-hero.buildXY('fence', 29, 30)
 # Вернись и построй частокол, чтобы защититься от огров.
+hero.moveXY(25, 30)
+hero.buildXY("fence", 30, 30)
+
