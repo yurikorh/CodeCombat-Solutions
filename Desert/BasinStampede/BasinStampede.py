2d0e11e90387d9118c9445a9bee42e132542acfe
yurikorh <33363503+yurikorh@users.noreply.github.com>
2021-11-29 19:21:32 +0800

    Update BasinStampede.py

--- Desert/BasinStampede/BasinStampede.py
+++ Desert/BasinStampede/BasinStampede.py
@@ -1,10 +1,15 @@
 while True:
     enemy = hero.findNearestEnemy()
-    xPos = hero.pos.x + 10
+    xPos = hero.pos.x + 5
     yPos = 17
     if enemy:
-        if enemy.pos.y > 17:
-            yPos = 10
-        elif enemy.pos.y <= 17:
-            yPos = 25
+        # 调整y值，使其向上或向下以摆脱牦牛。
+        if enemy.pos.y > hero.pos.y:
+            # 如果牦牛在你上面，从yPos中减去3。
+            yPos -= 3
+            pass
+        elif enemy.pos.y < hero.pos.y:
+            # 如果牦牛在你下面，给yPos加上3。
+            yPos += 3
+            pass
     hero.moveXY(xPos, yPos)
