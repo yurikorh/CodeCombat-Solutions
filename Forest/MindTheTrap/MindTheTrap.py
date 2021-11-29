43e105a44eebca5aae31f51fac77268883f5e00d
yurikorh <33363503+yurikorh@users.noreply.github.com>
2021-11-29 17:54:31 +0800

    Update MindTheTrap.py

--- Forest/MindTheTrap/MindTheTrap.py
+++ Forest/MindTheTrap/MindTheTrap.py
@@ -1,15 +1,7 @@
 # Если ты попробуешь атаковать врага, твой герой будет это делать, игнорируя все флаги. 
 # Ты должен убедиться, что ты атакуешь врагов, которые находятся рядом с тобой!
-
+# without flags
 while True:
-    flag = hero.findFlag()
     enemy = hero.findNearestEnemy()
-    if enemy:
-        dist = hero.distanceTo(enemy)
-    if flag:
-        # Возьми флаг.
-        hero.pickUpFlag(flag)
-        hero.say("Я должен взять флаг.")
-    elif enemy:
-        if (dist < 10):
-            hero.attack(enemy)
+    if enemy and enemy.pos.x < 50:
+        hero.attack(enemy)
