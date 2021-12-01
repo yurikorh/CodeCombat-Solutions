2eb4014f2e2419e7313bd94656b5e74afee45e8a
yurikorh <33363503+yurikorh@users.noreply.github.com>
2021-12-01 19:58:52 +0800

    Update BombingRun.py

--- Glacier/BombingRun/BombingRun.py
+++ Glacier/BombingRun/BombingRun.py
@@ -6,10 +6,7 @@
 while True:
     enemy = hero.findNearestEnemy()
     if enemy and hero.distanceTo(enemy) < 70:
-        O = Math.abs(enemy.pos.y - hero.pos.y)
-        A = Math.abs(enemy.pos.x - hero.pos.x)
-        angle = Math.atan2(O, A)
-        angle = angle * 180 / Math.PI
-        if enemy.pos.x < hero.pos.x:
-            angle = 180 - angle
-        hero.say(angle)
+        vector = Vector.subtract(enemy.pos, hero.pos)
+        rad = Math.atan2(vector.y, vector.x)
+        deg = rad * (180 / Math.PI)
+        hero.say(deg)
